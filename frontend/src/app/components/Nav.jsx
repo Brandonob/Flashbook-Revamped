696a229f567a4fb368238f874016a6fd32e9227d
Brandon Brown <brandonbrown@pursuit.org>
2025-04-23 16:29:24 -0400

    generate empty nav component and install chakra ui

--- frontend/src/app/components/Nav.jsx
+++ frontend/src/app/components/Nav.jsx
@@ -1,7 +1,19 @@
+'use client'
 import React from 'react'
+import { Box } from '@chakra-ui/react';
 
 export const Nav = () => {
   return (
-    <div>Nav</div>
+    <Box
+      as="nav"
+      position="sticky"
+      top="0"
+      width="100%"
+      bg="white"
+      boxShadow="md"
+      zIndex="1000"
+    >
+      {/* Add your nav items here */}
+    </Box>
   )
 }
