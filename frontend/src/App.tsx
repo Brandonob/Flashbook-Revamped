28d44c59cc9326972c4b614fe7728da43f499eb4
Brandon Brown <brandonbrown@pursuit.org>
2025-04-24 22:47:58 -0400

    create fixed nav container

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,13 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Nav } from './components/Nav'
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div id="appContainer">
       <Nav />
-    </>
+    </div>
   )
 }
 
--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -1,6 +1,6 @@
 // import React from 'react'
 export const Nav = () => {
   return (
-    <div>Nav</div>
+    <div id="navContainer">Nav</div>
   )
 }
