8f68df38fe5abac4ed6b582bebd6f1b7a54eff24
Brandon Brown <brandonbrown@pursuit.org>
2025-04-28 15:08:00 -0400

    add icons to right side of nav and add hover effect

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -1,5 +1,9 @@
-import fbLogo from '../assets/fbLogo.png'
-
+import fbIcon from '../assets/fbIcon.png'
+import menuIcon from '../assets/menuIcon.png'
+import messageIcon from '../assets/messageIcon.png'
+import notificationIcon from '../assets/notificationIcon.png'
+import avatarIcon from '../assets/avatarIcon.png'
+import thunderIcon from '../assets/thunderIcon.png'
 // import React from 'react'
 export const Nav = () => {
     const searchBar = () => {
@@ -13,11 +17,25 @@ export const Nav = () => {
   return (
     <div id="navContainer">
         <section id="navLeft">
-            <img src={fbLogo} alt="logo" className='fbLogo' />
+            <img src={fbIcon} alt="logo" className='fbIcon' />
             {searchBar()}
         </section>
         <section id="navCenter"></section>
-        <section id="navRight"></section>
+        <section id="navRight">
+            <section className='navRightIcon'>
+                <img src={menuIcon} alt="logo" className='menuIcon' />
+            </section>
+            <section className='navRightIcon'>
+                <img src={messageIcon} alt="logo" className='messageIcon' />
+                <img src={thunderIcon} alt="logo" className='thunderIcon' />
+            </section>
+            <section className='navRightIcon'>
+                <img src={notificationIcon} alt="logo" className='notificationIcon' />
+            </section>
+            <section className='navRightIcon'>
+                <img src={avatarIcon} alt="logo" className='profileIcon' />
+            </section>
+        </section>
     </div>
   )
 }
