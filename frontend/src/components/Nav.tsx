f429603f80ccdf528e83e3c2e8695a15249d542c
Brandon Brown <brandonbrown@pursuit.org>
2025-04-28 13:51:58 -0400

    add fb logo and searchbar to Nav component

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -1,8 +1,21 @@
+import fbLogo from '../assets/fbLogo.png'
+
 // import React from 'react'
 export const Nav = () => {
+    const searchBar = () => {
+        return (
+            <div id="searchBar">
+                <input className='searchInput' type="text" placeholder="Search" />
+            </div>
+        )
+    }
+
   return (
     <div id="navContainer">
-        <section id="navLeft"></section>
+        <section id="navLeft">
+            <img src={fbLogo} alt="logo" className='fbLogo' />
+            {searchBar()}
+        </section>
         <section id="navCenter"></section>
         <section id="navRight"></section>
     </div>
